68c68c056b1d7f177326bac05cae18592fb84a8f
Josh Hertzler <joshhertzler@gmail.com>
2023-04-25 11:13:45 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/* cloud {
+    organization = "joshua-hertzler"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
